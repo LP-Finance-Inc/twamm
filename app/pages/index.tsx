935b3bcf069ef219d9539533072ffd01c93d06b8
deepakkurmi5 <deepakkurmi874@gmail.com>
2023-05-03 16:11:33 +0530

    completed passcode

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,18 +1,55 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import IndexPage from "../src/pages/index-page";
+import { useState, useRef } from "react";
 
-const Home: NextPage = () => (
-  <>
-    <Head>
-      <meta
-        name="viewport"
-        content="initial-scale=1,user-scalable=no,maximum-scale=1,width=device-width"
-      />
-      <title>LP Finance | TWAMM</title>
-    </Head>
-    <IndexPage />
-  </>
-);
+import IndexPage from "../src/pages/index-page";
+import AnonPage from "../src/pages/anon-page";
+import { useSnackbar } from "../src/contexts/notification-context";
+
+const Home: NextPage = () => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const [isLogged, setIsLogged] = useState<boolean>(false);
+  const ref = useRef<any>(null);
+
+  const handleInputValue = (event: any) => {
+    if (event.key === "Enter") {
+      const value = ref.current.getInputValue();
+      const twammPassword = localStorage.getItem("twamm-access");
+      if (twammPassword) {
+        if (value === twammPassword) {
+          setIsLogged(true);
+          enqueueSnackbar("Welcome our twamm app", {
+            variant: "success",
+          });
+        } else {
+          enqueueSnackbar("Password not matched!", {
+            variant: "error",
+          });
+        }
+      } else {
+        localStorage.setItem("twamm-access", value);
+        setIsLogged(true);
+      }
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="initial-scale=1,user-scalable=no,maximum-scale=1,width=device-width"
+        />
+        <title>LP Finance | TWAMM</title>
+      </Head>
+      {isLogged ? (
+        <IndexPage />
+      ) : (
+        <AnonPage handleInputValue={handleInputValue} ref={ref} />
+      )}
+    </>
+  );
+};
 
 export default Home;
