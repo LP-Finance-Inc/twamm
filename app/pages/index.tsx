4f77bde23163406d5fdcd598dc270f060495d00a
aperire <aperire0402@gmail.com>
2023-05-21 23:11:02 -0400

    add preview

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -9,6 +9,17 @@ const Home: NextPage = () => (
         name="viewport"
         content="initial-scale=1,user-scalable=no,maximum-scale=1,width=device-width"
       />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="LP Finance | TWAMM" />
+      <meta
+        property="og:description"
+        content="Permissionless TWAP Order Execution"
+      />
+      <meta
+        property="og:image"
+        content="https://lptokenbucket.s3.amazonaws.com/Screenshot+2023-05-21+at+11.08.54+PM.png"
+      />
+      <meta property="og:url" content="https://twap.lp.finance" />
       <title>LP Finance | TWAMM</title>
     </Head>
     <IndexPage />
